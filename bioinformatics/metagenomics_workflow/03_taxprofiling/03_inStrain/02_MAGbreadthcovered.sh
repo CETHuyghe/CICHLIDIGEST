540fa95208d3647163d4bdaa2e0d388ed7aa12df
cethuy <131274400+CETHuyghe@users.noreply.github.com>
2026-09-15 16:14:40 +0200

    Update and rename 02_MAGbreathcovered.sh to 02_MAGbreadthcovered.sh

---
+++ bioinformatics/metagenomics_workflow/03_taxprofiling/03_inStrain/02_MAGbreadthcovered.sh
@@ -0,0 +1,10 @@
+# Script to extract the breadth of each reference MAG covered by sample reads, seperate file for each sample
+
+paste $(cat Sample_ID.txt | while IFS= read -r line || [[ -n "$line" ]];
+do 
+  {
+    echo "$line"
+    cut -f1,3 "OUT_DIR/$line/output/${line}_genome_info.tsv"
+  } > "OUT_DIR/genome_infos/${line}_column_breadth.txt"
+  echo "${line}_column_breadth.txt"
+done < Sample_ID.txt)
